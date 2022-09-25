a6f34b3e5a1f65579d5dc6533b971d4ff0ee13bb
root <root@DESKTOP-7PESA40.localdomain>
2022-09-26 02:10:16 +0900

    [추가, 테스트] ImageNode.cpp/.h 및 main.cpp .csv 읽기 테스트 성공

---
+++ ImageNode.cpp
@@ -0,0 +1,3 @@
+// 7P CSV 파일을 읽어 링크드 리스트 자료구조에 모두 저장을 위한 ImageNode.cpp
+
+#include "ImageNode.h"
--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include "Manager.h"
+#include<iostream>
+using namespace std;
+#include<fstream>
+#include<string>
 
 int main(int argc, char* argv[])
 {
-    const char* commandFilepath = "command.txt";
-    if (argc > 1)
-        commandFilepath = argv[1];
+    // const char* commandFilepath = "command.txt";
+    // if (argc > 1)
+    //     commandFilepath = argv[1];
 
-    Manager m;
-    m.Run(commandFilepath);
+    // Manager m;
+    // m.Run(commandFilepath);
+
+
+    string str;
+    fstream fs;
+    fs.open("img_files/filesnumbers.csv", ios::in);
+
+    while(!fs.eof()){
+        getline(fs, str, ',');
+        cout<<str<<endl;
+    }
+    fs.close();
+    
+    
+    ifstream in("command.txt");
+    if(!in)
+        cout<<"Unable to open command.txt"<<endl;
+    else{
+        
+    }
+
 
     return 0;
 } 
